9fcd37556a7405853567ee7c63e771696a684718
mickael_dutra <mickaeldutra99@gmail.com>
2025-12-09 00:53:54 -0300

    add faq

--- src/script/main.js
+++ src/script/main.js
@@ -1,7 +1,7 @@
 const buttons = document.querySelectorAll('[data-tab-button]');
+const questions = document.querySelectorAll('[data-faq-question]')
+
 document.addEventListener('DOMContentLoaded', function () {
-
-
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function (button) {
             const navTarget = button.target.dataset.tabButton;
@@ -12,7 +12,18 @@ document.addEventListener('DOMContentLoaded', function () {
             button.target.classList.add('show__tabs__button--is-active');
         })
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', openOrCloseAnswer)
+    }
 })
+
+function openOrCloseAnswer(element) {
+    const classe = 'faq__questions__item--is-open';
+    const fatherElement = element.target.parentNode;
+
+    fatherElement.classList.toggle(classe);
+}
 
 function removeButtonActive() {
     for (let i = 0; i < buttons.length; i++) {
