0cbb815d1ddd4fed1dca2b84dc1ef72bd3bb7e94
mickael_dutra <mickaeldutra99@gmail.com>
2025-12-09 16:13:05 -0300

    finalizando header

--- src/script/main.js
+++ src/script/main.js
@@ -1,5 +1,19 @@
 const buttons = document.querySelectorAll('[data-tab-button]');
 const questions = document.querySelectorAll('[data-faq-question]')
+const heroSection = document.querySelector('.hero');
+const heightHero = heroSection.clientHeight;
+
+
+window.addEventListener('scroll', function () {
+    const position = window.scrollY;
+
+    if (position < heightHero) {
+        hideHeader()
+    } else {
+        showHeader()
+    }
+});
+
 
 document.addEventListener('DOMContentLoaded', function () {
     buttons.forEach(button => {
@@ -9,14 +23,14 @@ document.addEventListener('DOMContentLoaded', function () {
     questions.forEach(question => {
         question.addEventListener('click', openOrCloseAnswer)
     })
-})
+});
 
 //funcionalidades da section class="faq"
 function openOrCloseAnswer(element) {
     const classFaq = 'faq__questions__item--is-open';
     const fatherElement = element.target.parentNode;
     fatherElement.classList.toggle(classFaq);
-}
+};
 //funcionalidades da section class="faq"
 
 //funcionalidades da section class="show"
@@ -27,18 +41,30 @@ function selectShow(event) {
     removeButtonActive();
     nav.classList.add('show__list--is-active');
     event.target.classList.add('show__tabs__button--is-active');
-}
+};
 
 function removeButtonActive() {
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].classList.remove('show__tabs__button--is-active');
     }
-}
+};
 
 function hideNav() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
     for (let i = 0; i < tabsContainer.length; i++) {
         tabsContainer[i].classList.remove('show__list--is-active');
     }
-}
+};
 //funcionalidades da section class="show"
+
+
+//funcionalidades do header class="header"
+function hideHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+};
+
+function showHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
